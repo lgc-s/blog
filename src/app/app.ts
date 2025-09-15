70f215e5c908ff3fdb15d1dbb220ceb166cf58cc
lgc-s <luisguilhermecs3@gmail.com>
2025-09-15 17:05:10 -0300

    post template added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Text } from './text/text';
+import { Button } from './button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Text],
+  imports: [Button, RouterOutlet, Text],
   templateUrl: './app.html',
 })
 export class App {
